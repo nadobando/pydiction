7982334a0b1e511224b555607ef6a4b17b9cc349
nadobando <7695172+nadobando@users.noreply.github.com>
2023-09-17 03:50:48 +0300

    no message

--- pydiction/__init__.py
+++ pydiction/__init__.py
@@ -2,3 +2,4 @@ from .core import Contains, DoesntContains, Matcher
 from .operators import ANY, ANY_NOT_NONE, Expect, ExpectNot
 
 __all__ = ["ANY", "ANY_NOT_NONE", "Matcher", "Contains", "DoesntContains", "Expect", "ExpectNot"]
+__version__ = "0.1.0"
