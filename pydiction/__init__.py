f696717c9a007dbb93229dd0e7af740932103a63
nadobando <7695172+nadobando@users.noreply.github.com>
2023-09-17 14:33:37 +0300

    build: preparing ci

--- pydiction/__init__.py
+++ pydiction/__init__.py
@@ -1,5 +1,5 @@
 from .core import Contains, DoesntContains, Matcher
 from .operators import ANY, ANY_NOT_NONE, Expect, ExpectNot
 
+__version__ = "0.0.0"
 __all__ = ["ANY", "ANY_NOT_NONE", "Matcher", "Contains", "DoesntContains", "Expect", "ExpectNot"]
-__version__ = "0.1.0"
